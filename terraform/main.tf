4418dbb037caa55748befbdb0feb7f057b7d1e36
root <root@webserver.akcedtbtsh1udlxzhpifd4bylg.tx.internal.cloudapp.net>
2026-08-08 06:49:03 +0000

    Add Terraform Infrastructure as Code (IaC) templates for 3-tier architecture

---
+++ terraform/main.tf
@@ -0,0 +1,105 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# 1. Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-three-tier-app"
+  location = "East US"
+}
+
+# 2. Virtual Network (VNet 10.0.0.0/16)
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-three-tier"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# 3. Public Subnet (10.0.1.0/24) for Web/App Tier
+resource "azurerm_subnet" "public_subnet" {
+  name                 = "subnet-public-web"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# 4. Private Subnet (10.0.2.0/24) for Database Tier
+resource "azurerm_subnet" "private_subnet" {
+  name                 = "subnet-private-db"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+# 5. Network Security Group (NSG) for Web Subnet
+resource "azurerm_network_security_group" "web_nsg" {
+  name                = "nsg-web-tier"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix       = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 110
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix       = "Internet"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 120
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix       = "Internet"
+    destination_address_prefix = "*"
+  }
+}
+
+# 6. Network Security Group (NSG) for DB Subnet
+resource "azurerm_network_security_group" "db_nsg" {
+  name                = "nsg-db-tier"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "MySQL"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3306"
+    source_address_prefix      = "10.0.1.0/24" # Restricted to Web Subnet
+    destination_address_prefix = "*"
+  }
+}
